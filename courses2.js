47f01a1de22b48a04c0e5f52fd79e4764327e4d7
Nick <84956743974@mail.ru>
2022-07-12 13:10:45 +0300

    new mini app for films

---
+++ courses2.js
@@ -0,0 +1,16 @@
+﻿const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+const lastWatchingFilms = prompt("Один из последних просмотренных фильмов", ""),
+      evaluatingTheFilm = +prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[lastWatchingFilms] = evaluatingTheFilm;
+
+console.log(personalMovieDB);
